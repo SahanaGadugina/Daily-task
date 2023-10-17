518cef4087425ba6f950ac339197716e97f7a81a
SahanaGadugina <sahanagadugina.xworkz@gmail.com>
2023-10-17 17:39:50 +0530

    Vehicle example

---
+++ Inheritance simple examles/VehicleMain.java
@@ -0,0 +1,10 @@
+class VehicleMain
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Starting main in VehicleMain");
+		Engine vehicle=new Engine();
+		
+		System.out.println("Ending main in VehicleMain");
+	}
+}
